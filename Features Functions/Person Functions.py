984e1baf87123e8ac9334a49f3aa0d90b97e4401
Tashbulat <dima@mind-set.ru>
2020-03-26 19:36:28 +0500

    Person Functions #1

---
+++ Features Functions/Person Functions.py
@@ -0,0 +1,127 @@
+
+import pandas as pd
+import numpy as np
+
+
+# Функция корректировки возраста
+
+def f_age( age ):
+    if pd.isnull(age):
+        age = 18
+    elif age < 18:
+        age = 18
+    elif age > 70:
+        age = 70
+    return age
+
+
+# Функция корректировки стажа
+
+def f_exp( exp ):
+    if pd.isnull(exp):
+        exp = 0
+    elif exp < 0:
+        exp = 0
+    elif exp > 52:
+        exp = 52
+    return exp
+
+
+# Функция определения пола
+
+def f_gender( client_type, client_name, client_gender='not_defined' ):
+
+    if client_type == 'company': #juridic person
+        gender_m = 0
+        gender_f = 0
+
+    elif client_gender == 'm':
+        gender_m = 1
+        gender_f = 0
+
+    elif client_gender == 'f':
+        gender_m = 0
+        gender_f = 1
+
+    else:
+        try:
+            if len(client_name) < 2:
+                gender_m = 0
+                gender_f = 0
+            elif client_name[-2:].upper() == 'ИЧ':
+                gender_m = 1
+                gender_f = 0
+            elif client_name[-2:].upper() == 'ОГЛЫ':
+                gender_m = 1
+                gender_f = 0
+            elif client_name[-2:].upper() == 'НА':
+                gender_m = 0
+                gender_f = 1
+            elif client_name[-2:].upper() == 'КЫЗЫ':
+                gender_m = 0
+                gender_f = 1
+            else:
+                gender_m = 0
+                gender_f = 0
+        except:
+            gender_m = 0
+            gender_f = 0
+
+    return gender_m, gender_f
+
+
+# Функция формирования признака пол-возраст-стаж
+
+def f_gen_age_exp( gender_m, gender_f, age, exp ):
+
+    if gender_m == 1:
+        gender = 1
+    elif gender_f == 1:
+        gender = 2
+    else:
+        gender = 0
+
+    if age < 25:
+        age = 0
+    elif age < 30:
+        age = 1
+    elif age < 35:
+        age = 2
+    elif age < 40:
+        age = 3
+    elif age < 45:
+        age = 4
+    elif age < 50:
+        age = 5
+    else:
+        age = 6
+
+    if exp < 5:
+        exp = 0
+    elif exp < 10:
+        exp = 1
+    elif exp < 15:
+        exp = 2
+    elif exp < 20:
+        exp = 3
+    elif exp < 25:
+        exp = 4
+    elif exp < 30:
+        exp = 5
+    else:
+        exp = 6
+
+    return gender*100 + age*10 + exp
+
+
+# Функция полиномайзер
+
+def f_polynomizer( data, column, n=2 ):
+    for i in range(2,n+1):
+        data[ column + '_' + str(i) ] = data[column]**i
+    return data
+
+
+
+
+
