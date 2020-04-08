1a9953d685b7734dda34d1e9e93e660264e62e40
Aleksandr Shchegolev <work@email.com>
2020-04-08 09:34:03 +0300

    Added documentation to defined functions.

--- scripts/hyperopt.py
+++ scripts/hyperopt.py
@@ -4,24 +4,58 @@ from hyperopt import hp, tpe, space_eval
 from hyperopt.fmin import fmin
 from sklearn.model_selection import train_test_split
 
-# Определим функцию Deviance для распределения Пуассона
+
+def train_val_test_split(x, y, val_size, test_size, random_state=0, shuffle=True, stratify=None):
+    """
+    Function for splitting dataset into train/validation/test partitions.
+
+    :param x: np.array object containing predictors
+    :param y: np.array object containing target variable
+    :param val_size: float, The proportion of the dataset to include in validation partition
+    :param test_size: float, The proportion of the dataset to include in test partition
+    :param random_state: int, optional (default=0). Passed to train_test_split() from scikit-learn.
+    :param shuffle: boolean, optional (default=True). Passed to train_test_split() from scikit-learn.
+    :param stratify: array-like or None (default=None). Passed to train_test_split() from scikit-learn.
+    :return: tuple, splitting (x_train, x_valid, x_test, y_train, y_valid, y_test)
+    """
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=random_state, shuffle=shuffle,
+                                                        test_size=test_size, stratify=stratify)
+    x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, random_state=random_state, shuffle=shuffle,
+                                                          test_size=val_size/(1-test_size), stratify=stratify)
+    return x_train, x_valid, x_test, y_train, y_valid, y_test
 
 
 def xgb_eval_dev_poisson(yhat, dtrain):
+    """
+    Function for Poisson Deviance evaluation
+
+    :param yhat: np.ndarray object with predictions
+    :param dtrain: xgb.DMatrix object with target variable
+    :return: (str, float), tuple with metrics name and its value
+    """
     y = dtrain.get_label()
     return 'dev_poisson', 2 * np.sum(y*np.log(y/yhat) - (y-yhat))
 
-# Определим функцию Deviance для распределения Гамма
-
 
 def xgb_eval_dev_gamma(yhat, dtrain):
+    """
+    Function for Gamma Deviance evaluation
+
+    :param yhat: np.ndarray object with predictions
+    :param dtrain: xgb.DMatrix object with target variable
+    :return: (str, float), tuple with metrics name and its value
+    """
     y = dtrain.get_label()
     return 'dev_gamma', 2 * np.sum(-np.log(y/yhat) + (y-yhat)/yhat)
 
-# Определим функцию для оптимизации гиперпараметров алгоритмом TPE
-
 
 def objective_xgb(params):
+    """
+    Objective function for hyperopt. Optimizing mean cross-validation error with XGBoost.
+
+    :param params: dict object passed to hyperopt fmin() function
+    :return: float, mean cross-validation error for XGBoost utilizing params
+    """
     params['max_depth'] = int(params['max_depth'])
     n_b_r = int(params.pop('num_boost_round'))
     data = params.pop('data')
@@ -34,6 +68,27 @@ def objective_xgb(params):
     name, _ = feval(data.get_label(), data)
     score = cv_result['test-{}-mean'.format(name)][-1:].values[0]
     return score
+
+
+def train_xgb_best_params(params, dtrain, evals, early_stopping_rounds, evals_result=None, verbose_eval=None):
+    """
+    Function to train XGBoost estimator from set of parameters, passed from hyperopt.
+
+    :param params: dict, hyperparameters from hyperopt space_eval() function
+    :param dtrain: xgb.DMatrix object, to train model on
+    :param evals: list of pairs (DMatrix, str). Same from xgb.train().
+    :param early_stopping_rounds: int. Same from xgb.train().
+    :param evals_result: dict. Same from xgb.train().
+    :param verbose_eval: bool or int. Same from xgb.train().
+    :return: xgb.Booster object, trained model
+    """
+    for label in ['nfold', 'data', 'early_stopping_rounds']:
+        del params[label]
+    n_b_r = int(params.pop('num_boost_round'))
+    maximize = params.pop('maximize')
+    feval = params.pop('feval')
+    return xgb.train(params=params, dtrain=dtrain, num_boost_round=n_b_r, evals=evals, feval=feval, maximize=maximize,
+                     early_stopping_rounds=early_stopping_rounds, evals_result=evals_result, verbose_eval=verbose_eval)
 
 # Определим границы, в которых будем искать гиперпараметры
 space_freq = {'data': train_freq,
@@ -69,33 +124,9 @@ space_avgclm = {'data': train_avgclaim,
                 }
 
 # Оптимизация
-
-
 model_freq_best = fmin(fn=objective_xgb, space=space_freq, algo=tpe.suggest, max_evals=50)
 model_avclaim_best = fmin(fn=objective_xgb, space=space_avgclm, algo=tpe.suggest, max_evals=50)
 
 # Оптимальные гиперпараметры
-
-
 best_params_freq = space_eval(space_freq, model_freq_best)
 best_params_avclaim = space_eval(space_avgclm, model_avclaim_best)
-
-# Функция для обучения модели
-
-
-def train_xgb_best_params(params, dtrain, evals, early_stopping_rounds, evals_result=None, verbose_eval=None):
-    for label in ['nfold', 'data', 'early_stopping_rounds']:
-        del params[label]
-    n_b_r = int(params.pop('num_boost_round'))
-    maximize = params.pop('maximize')
-    feval = params.pop('feval')
-    return xgb.train(params=params, dtrain=dtrain, num_boost_round=n_b_r, evals=evals, feval=feval, maximize=maximize,
-                     early_stopping_rounds=early_stopping_rounds, evals_result=evals_result, verbose_eval=verbose_eval)
-
-
-def train_val_test_split(x, y, val_size, test_size, random_state=0, shuffle=True, stratify=None):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=random_state, shuffle=shuffle,
-                                                        test_size=test_size, stratify=stratify)
-    x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, random_state=random_state, shuffle=shuffle,
-                                                          test_size=val_size/(1-test_size), stratify=stratify)
-    return x_train, x_valid, x_test, y_train, y_valid, y_test
