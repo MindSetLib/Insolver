ae3ba2a9069dfd94c86205e9e6b4463e9a391f5d
Andrey Anokhin <anokhin.andrey@gmail.com>
2020-09-15 17:58:29 +0300

    add train and inference examples

---
+++ examples/train.py
@@ -0,0 +1,62 @@
+import pickle
+
+import pandas as pd
+
+from insolver.InsolverDataFrame import InsolverDataFrame
+from insolver.InsolverTransforms import (
+    TransformExp,
+    InsolverTransforms,
+    TransformAge,
+    TransformMapValues,
+    TransformPolynomizer,
+    TransformAgeGender,
+)
+from insolver.InsolverUtils import train_val_test_split
+from insolver.InsolverWrapperGLM import InsolverGLMWrapper
+
+df = pd.read_csv('freMPL-R.csv', low_memory=False)
+
+InsDataFrame = InsolverDataFrame(df)
+
+InsTransforms = InsolverTransforms(InsDataFrame.get_data(), [
+    TransformAge('DrivAge', 18, 75),
+    TransformExp('LicAge', 57),
+    TransformMapValues('Gender', {'Male': 0, 'Female': 1}),
+    TransformMapValues('MariStat', {'Other': 0, 'Alone': 1}),
+    TransformAgeGender('DrivAge', 'Gender', 'Age_m', 'Age_f', age_default=18, gender_male=0, gender_female=1),
+    TransformPolynomizer('Age_m'),
+    TransformPolynomizer('Age_f'),
+])
+
+transforms = InsTransforms.transform()
+
+with open('transforms.pkl', 'wb') as file:
+    pickle.dump(transforms, file)
+
+df = InsTransforms.get_data()
+df = df[df.Dataset.isin([5, 6, 7, 8, 9])]
+df.dropna(how='all', axis=1, inplace=True)
+df = df[df.ClaimAmount > 0]
+
+iglm = InsolverGLMWrapper()
+
+params = {'lambda': [1, 0.5, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0],
+          'alpha': [i * 0.1 for i in range(0, 11)]}
+features = ['LicAge', 'Gender', 'MariStat', 'DrivAge', 'HasKmLimit', 'BonusMalus', 'RiskArea',
+            'Age_m', 'Age_f', 'Age_m_2', 'Age_f_2']
+target = 'ClaimAmount'
+
+x_train, x_valid, x_test, y_train, y_valid, y_test = train_val_test_split(
+    df[features], df[target], val_size=0.7, test_size=0.15)
+
+train = pd.concat([x_train, y_train], axis=1)
+valid = pd.concat([x_valid, y_valid], axis=1)
+test = pd.concat([x_test, y_test], axis=1)
+
+test.sample(1).to_json('request_example.json')
+
+train, valid = iglm.model_init(train, valid, family='gamma', link='log')
+iglm.grid_search_cv(features, target, train, valid, params, search_criteria={'strategy': "Cartesian"})
+predict_glm = iglm.predict(test)
+iglm.save_model('glm')
+print(predict_glm)
