f7ca174486fb5a42fb0ad417a5f5755cfa8ac5e5
Andrey Anokhin <anokhin.andrey@gmail.com>
2020-10-15 23:35:36 +0300

    add save_json method to InsolverTransforms

--- insolver/InsolverTransforms.py
+++ insolver/InsolverTransforms.py
@@ -65,6 +65,10 @@ class InsolverTransforms(InsolverDataFrame):
     def save(self, filename):
         with open(filename, 'wb') as file:
             pickle.dump(self.transforms_done, file)
+            
+    def save_json(self, filename):
+        with open(filename, 'w') as file:
+            json.dump(self.transforms_done, file, separators=(',', ':'), sort_keys=True, indent=4)
 
 
 # ---------------------------------------------------
