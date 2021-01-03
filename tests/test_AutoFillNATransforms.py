9db617d47fa739b1ddd7ce5b2d14eeaee1c33eec
Andrey Anokhin <anokhin.andrey@gmail.com>
2021-01-03 15:21:01 +0300

    fix

--- tests/test_AutoFillNATransforms.py
+++ tests/test_AutoFillNATransforms.py
@@ -8,7 +8,7 @@ from insolver.transforms import InsolverTransform, AutoFillNATransforms
 def test_fillna_numerical():
     df_test = InsolverDataFrame(pd.DataFrame(data={'col1': [1, 2, np.nan]}))
     InsTransforms = InsolverTransform(df_test, [
-        AutoFillNATransforms('col1'),
+        AutoFillNATransforms(),
     ])
     InsTransforms.ins_transform()
     assert df_test['col1'][2] == 1.5
@@ -17,7 +17,7 @@ def test_fillna_numerical():
 def test_fillna_numerical_all_na():
     df_test = InsolverDataFrame(pd.DataFrame(data={'col1': [np.nan, np.nan, np.nan]}))
     InsTransforms = InsolverTransform(df_test, [
-        AutoFillNATransforms('col1'),
+        AutoFillNATransforms(),
     ])
     InsTransforms.ins_transform()
     assert df_test['col1'][0] == 1
@@ -28,7 +28,7 @@ def test_fillna_numerical_all_na():
 def test_fillna_categorical():
     df_test = InsolverDataFrame(pd.DataFrame(data={'col1': ['A', 'B', 'C', 'A', None]}))
     InsTransforms = InsolverTransform(df_test, [
-        AutoFillNATransforms('col1'),
+        AutoFillNATransforms(),
     ])
     InsTransforms.ins_transform()
     assert df_test['col1'][4] == 'A'
@@ -37,7 +37,7 @@ def test_fillna_categorical():
 def test_fillna_categorical_all_na():
     df_test = InsolverDataFrame(pd.DataFrame(data={'col1': [None, None, None]}))
     InsTransforms = InsolverTransform(df_test, [
-        AutoFillNATransforms('col1'),
+        AutoFillNATransforms(),
     ])
     InsTransforms.ins_transform()
     assert df_test['col1'][0] == 1
