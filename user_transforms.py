6363ade2b3e938ccefa6f87730a868e864391d28
Andrey Anokhin <anokhin.andrey@gmail.com>
2021-01-04 14:59:41 +0300

    refactor user_transforms

--- user_transforms.py
+++ user_transforms.py
@@ -1,20 +1,18 @@
 import pandas as pd
 
 
-class TransformSocioCateg:
-    def __init__(self, column_socio_categ):
-         self.column_socio_categ = column_socio_categ
-
-    def __call__(self, df):
-        df[self.column_socio_categ] = df[self.column_socio_categ].str.slice(0,4)
-        return df
-
-
 class TransformToNumeric:
-    def __init__(self, column_param, downcast='integer'):
-        self.column_param = column_param
+    """Example of user-defined transformations. Transform values to numeric.
+
+    Attributes:
+        column_names (list): List of columns for transformations
+        downcast (str): parameter from pd.to_numeric, default: 'float'
+    """
+    def __init__(self, column_names, downcast='float'):
+        self.column_names = column_names
         self.downcast = downcast
 
     def __call__(self, df):
-        df[self.column_param] = pd.to_numeric(df[self.column_param], downcast=self.downcast)
+        for column in self.column_names:
+            df[column] = pd.to_numeric(df[column], downcast=self.downcast)
         return df
