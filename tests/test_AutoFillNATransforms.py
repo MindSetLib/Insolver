82c376da95d9f3ab2d777131f5933faa26ba8730
Andrey Anokhin <anokhin.andrey@gmail.com>
2021-01-04 13:46:36 +0300

    fix tests

--- tests/test_AutoFillNATransforms.py
+++ tests/test_AutoFillNATransforms.py
@@ -7,39 +7,39 @@ from insolver.transforms import InsolverTransform, AutoFillNATransforms
 
 def test_fillna_numerical():
     df_test = InsolverDataFrame(pd.DataFrame(data={'col1': [1, 2, np.nan]}))
-    InsTransforms = InsolverTransform(df_test, [
+    df_transformed = InsolverTransform(df_test, [
         AutoFillNATransforms(),
     ])
-    InsTransforms.ins_transform()
-    assert df_test['col1'][2] == 1.5
+    df_transformed.ins_transform()
+    assert df_transformed['col1'][2] == 1.5
 
 
 def test_fillna_numerical_all_na():
     df_test = InsolverDataFrame(pd.DataFrame(data={'col1': [np.nan, np.nan, np.nan]}))
-    InsTransforms = InsolverTransform(df_test, [
+    df_transformed = InsolverTransform(df_test, [
         AutoFillNATransforms(),
     ])
-    InsTransforms.ins_transform()
-    assert df_test['col1'][0] == 1
-    assert df_test['col1'][1] == 1
-    assert df_test['col1'][2] == 1
+    df_transformed.ins_transform()
+    assert df_transformed['col1'][0] == 1
+    assert df_transformed['col1'][1] == 1
+    assert df_transformed['col1'][2] == 1
 
 
 def test_fillna_categorical():
     df_test = InsolverDataFrame(pd.DataFrame(data={'col1': ['A', 'B', 'C', 'A', None]}))
-    InsTransforms = InsolverTransform(df_test, [
+    df_transformed = InsolverTransform(df_test, [
         AutoFillNATransforms(),
     ])
-    InsTransforms.ins_transform()
-    assert df_test['col1'][4] == 'A'
+    df_transformed.ins_transform()
+    assert df_transformed['col1'][4] == 'A'
 
 
 def test_fillna_categorical_all_na():
     df_test = InsolverDataFrame(pd.DataFrame(data={'col1': [None, None, None]}))
-    InsTransforms = InsolverTransform(df_test, [
+    df_transformed = InsolverTransform(df_test, [
         AutoFillNATransforms(),
     ])
-    InsTransforms.ins_transform()
-    assert df_test['col1'][0] == 1
-    assert df_test['col1'][1] == 1
-    assert df_test['col1'][2] == 1
+    df_transformed.ins_transform()
+    assert df_transformed['col1'][0] == 1
+    assert df_transformed['col1'][1] == 1
+    assert df_transformed['col1'][2] == 1
--- tests/test_EncoderTransforms.py
+++ tests/test_EncoderTransforms.py
@@ -6,11 +6,11 @@ from insolver.transforms import InsolverTransform, EncoderTransforms
 
 def test_EncoderTransforms():
     df_test = InsolverDataFrame(pd.DataFrame(data={'col1': ['A', 'B', 'C', 'A']}))
-    InsTransforms = InsolverTransform(df_test, [
+    df_transformed = InsolverTransform(df_test, [
         EncoderTransforms(['col1']),
     ])
-    InsTransforms.ins_transform()
-    assert df_test['col1'][0] == 0
-    assert df_test['col1'][1] == 1
-    assert df_test['col1'][2] == 2
-    assert df_test['col1'][3] == 0
+    df_transformed.ins_transform()
+    assert df_transformed['col1'][0] == 0
+    assert df_transformed['col1'][1] == 1
+    assert df_transformed['col1'][2] == 2
+    assert df_transformed['col1'][3] == 0
