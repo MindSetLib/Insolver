d566c3805322f48a7eaa4e5d791955b732ab88a1
Andrey Anokhin <anokhin.andrey@gmail.com>
2021-02-02 07:36:43 +0300

    add test sample_request

--- tests/test_InsolverDataFrame.py
+++ tests/test_InsolverDataFrame.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from insolver import InsolverDataFrame
 
-
-df = pd.read_csv('tests/data/freMPL-R_100.csv')
+df = pd.DataFrame({'col1': [1], 'col2': [2]})
 
 
 def test_InsolverDataFrame():
@@ -10,3 +9,10 @@ def test_InsolverDataFrame():
     assert hasattr(InsDataFrame, 'get_meta_info')
     assert hasattr(InsDataFrame, 'get_batch')
     assert hasattr(InsDataFrame, 'split_frame')
+    assert hasattr(InsDataFrame, 'sample_request')
+
+
+def test_sample_request():
+    InsDataFrame = InsolverDataFrame(df)
+    request = InsDataFrame.sample_request()
+    assert request['df'] == {'col1': {'0': 1}, 'col2': {'0': 2}}
