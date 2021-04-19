31f6b92e840bcae4c48e962e53a1b302adaa9795
alexmindset <alex@mind-set.ru>
2021-04-19 18:41:55 +0300

    Update gain_curve

--- insolver/model_tools/model_utils.py
+++ insolver/model_tools/model_utils.py
@@ -3,8 +3,7 @@ from io import BytesIO
 from urllib.request import urlopen
 from zipfile import ZipFile
 
-from numpy import log, sum, maximum, unique, true_divide, linspace, ndarray
-
+import numpy as np
 import matplotlib.pyplot as plt
 
 from pandas import DataFrame, Series, concat, qcut
@@ -113,9 +112,9 @@ def deviance_poisson(y_hat, y, weight=None):
     """
     t_hat, t = y_hat + 1, y + 1
     if weight:
-        return sum(2 * weight * (t * log(t / t_hat) - (t - t_hat)))
+        return sum(2 * weight * (t * np.log(t / t_hat) - (t - t_hat)))
     else:
-        return sum(2 * (t * log(t / t_hat) - (t - t_hat)))
+        return sum(2 * (t * np.log(t / t_hat) - (t - t_hat)))
 
 
 def deviance_gamma(y_hat, y, weight=None):
@@ -130,9 +129,9 @@ def deviance_gamma(y_hat, y, weight=None):
         float, value of the Gamma deviance.
     """
     if weight:
-        return sum(2 * weight * (-log(y/y_hat) + (y-y_hat)/y_hat))
+        return sum(2 * weight * (-np.log(y/y_hat) + (y-y_hat)/y_hat))
     else:
-        return sum(2 * (-log(y/y_hat) + (y-y_hat)/y_hat))
+        return sum(2 * (-np.log(y/y_hat) + (y-y_hat)/y_hat))
 
 
 def inforamtion_value_woe(data, target, bins=10, cat_thresh=10, detail=False):
@@ -152,17 +151,17 @@ def inforamtion_value_woe(data, target, bins=10, cat_thresh=10, detail=False):
     target = target.name if isinstance(target, Series) else target
     cols = data.columns
     for ivars in cols[~cols.isin([target])]:
-        if (data[ivars].dtype.kind in 'bifc') and (len(unique(data[ivars])) > cat_thresh):
+        if (data[ivars].dtype.kind in 'bifc') and (len(np.unique(data[ivars])) > cat_thresh):
             binned_x = qcut(data[ivars], bins,  duplicates='drop')
             d0 = DataFrame({'x': binned_x, 'y': data[target]})
         else:
             d0 = DataFrame({'x': data[ivars], 'y': data[target]})
         d = d0.groupby("x", as_index=False).agg({"y": ["count", "sum"]})
         d.columns = ['Cutoff', 'N', 'Events']
-        d['% of Events'] = maximum(d['Events'], 0.5) / d['Events'].sum()
+        d['% of Events'] = np.maximum(d['Events'], 0.5) / d['Events'].sum()
         d['Non-Events'] = d['N'] - d['Events']
-        d['% of Non-Events'] = maximum(d['Non-Events'], 0.5) / d['Non-Events'].sum()
-        d['WoE'] = log(d['% of Events'] / d['% of Non-Events'])
+        d['% of Non-Events'] = np.maximum(d['Non-Events'], 0.5) / d['Non-Events'].sum()
+        d['WoE'] = np.log(d['% of Events'] / d['% of Non-Events'])
         d['IV'] = d['WoE'] * (d['% of Events'] - d['% of Non-Events'])
         d.insert(loc=0, column='Variable', value=ivars)
         temp = DataFrame({"Variable": [ivars], "IV": [d['IV'].sum()]}, columns=["Variable", "IV"])
@@ -171,20 +170,29 @@ def inforamtion_value_woe(data, target, bins=10, cat_thresh=10, detail=False):
     return short_result if detail else detailed_result
 
 
-def gain_curve(predict, exposure, step=1):
-    if isinstance(predict, (Series, ndarray)) and isinstance(exposure, Series):
+def gain_curve(predict, exposure, step=1, figsize=(10, 6), gini_exact=False, output=False):
+    gini_df = DataFrame()
+    plt.figure(figsize=figsize)
+    if isinstance(predict, (Series, np.ndarray)) and isinstance(exposure, Series):
         temp_df = concat([Series(predict, name='Predict').reset_index(drop=True),
                           exposure.reset_index(drop=True)], axis=1)
         temp_df = temp_df.sort_values('Predict', ascending=False).reset_index(drop=True)
         normalized_df = temp_df.cumsum()/temp_df.sum()
-        w = sum(temp_df[exposure.name])
-        m = true_divide(sum(temp_df[exposure.name] * temp_df['Predict']), sum(temp_df[exposure.name]))
-        temp_df['Rank'] = 0
-        temp_df.loc[0, 'Rank'] = 1 + 0.5 * (temp_df.loc[0, exposure.name] - 1)
-        for x in range(1, len(temp_df)):
-            temp_df.loc[x, 'Rank'] = (temp_df.loc[x-1, 'Rank'] + 0.5 * (temp_df.loc[x-1, exposure.name] + 1)
-                                      + 0.5 * (temp_df.loc[x, exposure.name] - 1))
-        gini = 1 + 1/w - 2/(w**2 * m) * sum(temp_df[exposure.name] * temp_df['Predict'] * temp_df['Rank'])
+        if gini_exact:
+            w = sum(temp_df[exposure.name])
+            m = np.true_divide(sum(temp_df[exposure.name] * temp_df['Predict']), sum(temp_df[exposure.name]))
+            temp_df['Rank'] = 0
+            temp_df.loc[0, 'Rank'] = 1 + 0.5 * (temp_df.loc[0, exposure.name] - 1)
+            for x in range(1, len(temp_df)):
+                temp_df.loc[x, 'Rank'] = (temp_df.loc[x-1, 'Rank'] + 0.5 * (temp_df.loc[x-1, exposure.name] + 1)
+                                          + 0.5 * (temp_df.loc[x, exposure.name] - 1))
+            gini = 1 + 1/w - 2/(w**2 * m) * sum(temp_df[exposure.name] * temp_df['Predict'] * temp_df['Rank'])
+        else:
+            auc = (np.sum(normalized_df['Predict'] * np.append(np.diff(normalized_df['Exposure']), 0))
+                   + np.sum(np.append(np.diff(normalized_df['Predict']), 0) *
+                            np.append(np.diff(normalized_df['Exposure']), 0))/2)
+            gini = 2 * auc - 1
+        gini_df = gini_df.append(DataFrame.from_dict({'Gini': {'Predict': gini}}))
         plt.plot(normalized_df[exposure.name].values[::step], normalized_df['Predict'].values[::step],
                  label=f'Predict (Gini: {round(gini, 3)})')
     elif isinstance(predict, DataFrame) and isinstance(exposure, Series):
@@ -192,21 +200,30 @@ def gain_curve(predict, exposure, step=1):
         for pred_col in temp_df.columns[:-1]:
             temp_df2 = temp_df[[pred_col, exposure.name]].sort_values(pred_col, ascending=False).reset_index(drop=True)
             normalized_df = temp_df2.cumsum()/temp_df2.sum()
-            w = sum(temp_df2[exposure.name])
-            m = true_divide(sum(temp_df2[exposure.name] * temp_df2[pred_col]), sum(temp_df2[exposure.name]))
-            temp_df2['Rank'] = 0
-            temp_df2.loc[0, 'Rank'] = 1 + 0.5 * (temp_df2.loc[0, exposure.name] - 1)
-            for x in range(1, len(temp_df2)):
-                temp_df2.loc[x, 'Rank'] = (temp_df2.loc[x-1, 'Rank'] + 0.5 * (temp_df2.loc[x-1, exposure.name] + 1)
-                                           + 0.5 * (temp_df2.loc[x, exposure.name] - 1))
-            gini = 1 + 1/w - 2/(w**2 * m) * sum(temp_df2[exposure.name] * temp_df2[pred_col] * temp_df2['Rank'])
+            if gini_exact:
+                w = sum(temp_df2[exposure.name])
+                m = np.true_divide(sum(temp_df2[exposure.name] * temp_df2[pred_col]), sum(temp_df2[exposure.name]))
+                temp_df2['Rank'] = 0
+                temp_df2.loc[0, 'Rank'] = 1 + 0.5 * (temp_df2.loc[0, exposure.name] - 1)
+                for x in range(1, len(temp_df2)):
+                    temp_df2.loc[x, 'Rank'] = (temp_df2.loc[x-1, 'Rank'] + 0.5 * (temp_df2.loc[x-1, exposure.name] + 1)
+                                               + 0.5 * (temp_df2.loc[x, exposure.name] - 1))
+                gini = 1 + 1/w - 2/(w**2 * m) * sum(temp_df2[exposure.name] * temp_df2[pred_col] * temp_df2['Rank'])
+            else:
+                auc = (np.sum(normalized_df[pred_col] * np.append(np.diff(normalized_df[exposure.name]), 0))
+                       + np.sum(np.append(np.diff(normalized_df[pred_col]), 0) *
+                                np.append(np.diff(normalized_df[exposure.name]), 0))/2)
+                gini = 2 * auc - 1
+            gini_df = gini_df.append(DataFrame.from_dict({'Gini': {pred_col: gini}}))
             plt.plot(normalized_df[exposure.name].values[::step], normalized_df[pred_col].values[::step],
                      label=f'{pred_col} (Gini: {round(gini, 3)})')
     else:
         raise Exception
     plt.legend()
-    plt.plot(linspace(0, 1, 2), linspace(0, 1, 2), c='red', linestyle='--', linewidth=0.7)
+    plt.plot(np.linspace(0, 1, 2), np.linspace(0, 1, 2), c='red', linestyle='--', linewidth=0.7)
     plt.title('Gains curve')
     plt.xlabel('Cumulative exposure')
     plt.ylabel('Cumulative response')
     plt.show()
+    if output:
+        return gini_df
