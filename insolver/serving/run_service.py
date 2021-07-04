497de7a3b54cf7d6aec7fc34c0a0dfd9ce076256
frank <frank@mind-set.ru>
2021-07-04 22:24:39 +0300

    Create serving files for flask api and fast api with several models

--- insolver/serving/run_service.py
+++ insolver/serving/run_service.py
@@ -13,6 +13,11 @@ def run():
     parser.add_argument('-model', action='store')
     parser.add_argument('-transforms', action='store')
     parser.add_argument('-service', action='store')
+    
+    #add new parameter config file and models and transforms
+    parser.add_argument('-configfile', action='store')
+    parser.add_argument('-transforms_folder', action='store')
+    parser.add_argument('-models_folder', action='store')
 
     # gunicorn config
     parser.add_argument('-ip', action='store', default='0.0.0.0')
@@ -22,12 +27,24 @@ def run():
 
     os.environ['model_path'] = args.model
     os.environ['transforms_path'] = args.transforms
-
+    
+    #add new config file and models
+    os.environ['config_file'] = args.configfile
+    os.environ['transforms_folder'] = args.transforms_folder
+    os.environ['models_folder'] = args.models_folder
+    
+    
     if args.service == 'flask':
         cmd = f'gunicorn -b {args.ip}:{args.port} insolver.serving.flask_app:app'
         exec_cmd(cmd)
     elif args.service == 'fastapi':
         cmd = f'gunicorn -b {args.ip}:{args.port} insolver.serving.fastapi_app:app -k uvicorn.workers.UvicornWorker'
+        exec_cmd(cmd)
+    if args.service == 'sflask':
+        cmd = f'gunicorn --worker-class gthread -b {args.ip}:{args.port} insolver.serving.flask_app_several:app'
+        exec_cmd(cmd)
+    elif args.service == 'sfastapi':
+        cmd = f'gunicorn -b {args.ip}:{args.port} insolver.serving.fastapi_app_several:app -k uvicorn.workers.UvicornWorker'
         exec_cmd(cmd)
     else:
         print('wrong service, try "-service flask" or "-service fastapi"')
