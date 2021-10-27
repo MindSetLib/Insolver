eeb68812ca032fc9e1d8001e0a6d27cc11b0b42f
Alim.U <alim.uzhbanokov@gmail.com>
2021-10-27 13:57:41 +0200

    Added CART method. Added docstrings

--- insolver/discretization/discretizer.py
+++ insolver/discretization/discretizer.py
@@ -1,65 +1,107 @@
-from numpy import log10, std, percentile, subtract
+from numpy import log10, std, percentile, subtract, sqrt, power, ndarray
 
 from insolver.discretization.discretizer_utils import *
 
 
 class InsolverDiscretizer:
     """
-    X: column
-    method: 'uniform', 'quantile', 'kmeans'
-    n_bins: int or str
+    Trasform continuous variable into discrete form
+
+    Parameters:
+    method: {'uniform', 'quantile', 'kmeans', 'cart'} method used to discretize
     """
 
-    _methods = {
-        'uniform': EqualWidthDiscretizer,
-        'quantile': EqualFrequencyDiscretizer,
-        'kmeans': KMeansDiscretizer,
-    }
+    _methods = ['uniform', 'quantile', 'kmeans', 'cart']
 
-    _n_bins_formula = ('square-root', 'sturgers', 'rice-rule',
+    _n_bins_formula = ('square-root', 'sturges', 'rice-rule',
                        'scotts-rule', 'freedman-diaconis')
 
-    def __init__(self, X, method='uniform', n_bins=5):
-        if method not in self._methods.keys():
+    def __init__(self, method='uniform'):
+        if method not in self._methods:
             raise ValueError(f'Accepted methods are {self._methods}, got {method} instead.')
 
-        if not ((type(n_bins) == int and n_bins > 1)
-                or n_bins in self._n_bins_formula):
-            raise ValueError(f'Invalid number of bins. '
-                             f'Accepted integer value or one of the following options: {self._n_bins_formula},'
-                             f'got {n_bins} instead.')
-
-        self.X = X
         self.method = method
-        self.n_bins = n_bins
+        self.X = None
 
-    def transform(self):
-        len_X = self.__check_data_shape()
+    def transform(self, X, y=None, n_bins=None, min_samples_leaf=None):
+        """
+        Apply discretization to given data
 
-        if self.n_bins in self._n_bins_formula:
-            self.n_bins = self.__calculate_n_bins(self.n_bins, len_X)
+        Args:
+            X: 1-D array, data to be descretized
+            y: 1-D array, target values, ignored for unsupervised transformations
+            n_bins: int or {'square-root', 'sturges', 'rice-rule', 'scotts-rule', 'freedman-diaconis'}
+            min_samples_leaf: int or float, for 'cart' method only, ignored otherwise
 
-        return self._methods[self.method]. \
-            _transform(self.X, self.n_bins)
+        Returns:
+            1-D array, transformed data
+        """
+        self.X = X
+
+        # if isinstance(self.X, list):  # TODO add proper way to control input array type
+        #     self.X = np.array(self.X)
+        # elif not isinstance(self.X, ndarray):
+        #     raise ValueError(f'Invalid array type'
+        #                      f'Expected ndarray or list, got {type(self.X)} instead')
+
+        if self.method in ['uniform', 'quantile', 'kmeans']:
+            if not ((isinstance(n_bins, int) and n_bins > 1)
+                    or n_bins in self._n_bins_formula):
+                raise ValueError(f'Invalid number of bins. '
+                                 f'Accepted integer value or one of the following options: {self._n_bins_formula},'
+                                 f'got {n_bins} instead.')
+
+            len_X = self.__check_data_shape()
+
+            if n_bins in self._n_bins_formula:
+                n_bins = self.__calculate_n_bins(n_bins, len_X)
+
+            return KBinsDiscretizer._transform(self.X, n_bins, self.method)
+
+        if self.method == 'cart':
+            # if y is None or not isinstance(y, np.ndarray): # TODO add proper way to control input target type
+            #     raise ValueError(f'Invalid target value. '
+            #                      f'Expected 1-D array, got {y} instead')
+            if len(y.shape) != 1 or y.shape[0] != self.X.shape[0]:
+                raise ValueError(f'Invalid target shape. '
+                                 f'Expected 1-D array with shape {(self.X.shape[0],)}, got {y.shape} instead')
+
+            return CARTDiscretizer._transform(self.X, y, min_samples_leaf)
 
     def __calculate_n_bins(self, n_bins, len_X):
+        """
+        Calculate number of bins
+
+        Args:
+            X: 1-D array, data to be descretized
+            n_bins: string, formula to calculate number of bins
+            len_X: int, length of X
+
+        Returns:
+            int, number of bins
+        """
         if n_bins == 'square-root':
-            return round(len_X ** 1 / 2)
-
-        if n_bins == 'sturgers':
+            return round(sqrt(len_X))
+        elif n_bins == 'sturges':
             return round(1 + 3.322 * log10(len_X))
-
-        if n_bins == 'rice-rule':
-            return round(len_X ** 1 / 3 * 2)
-
-        if n_bins == 'scotts-rule':
-            return round(3.49 * std(self.X) / len_X ** 1 / 3)
-
-        if n_bins == 'freedman-diaconis':
+        elif n_bins == 'rice-rule':
+            return round(power(len_X, 1/3) * 2)
+        elif n_bins == 'scotts-rule':
+            return round(3.49 * std(self.X) / power(len_X, 1/3))
+        elif n_bins == 'freedman-diaconis':
             iqr = subtract(*percentile(self.X, [75, 25]))
-            return round(2 * iqr / len_X ** 1 / 3)
+            return round(2 * iqr / power(len_X, 1/3))
 
     def __check_data_shape(self):
+        """
+        Check shape of X
+
+        Args:
+            X: 1-D array, data to be descretized
+
+        Returns:
+            int, length of X
+        """
         if len(self.X.shape) not in (1, 2):
             raise ValueError(f'Expected 1D or 2D array, '
                              f'got shape={self.X.shape} instead.')
@@ -67,8 +109,7 @@ class InsolverDiscretizer:
         if len(self.X.shape) == 1:
             self.X = self.X.reshape(-1, 1)
             return self.X.shape[0]
-
-        if len(self.X.shape) == 2:
+        elif len(self.X.shape) == 2:
             if self.X.shape[0] == 1:
                 self.X = self.X.reshape(-1, 1)
 
--- insolver/discretization/discretizer_utils.py
+++ insolver/discretization/discretizer_utils.py
@@ -1,22 +1,62 @@
-from sklearn.preprocessing import KBinsDiscretizer
+import numpy as np
+import sklearn.preprocessing
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
 
 
-class EqualWidthDiscretizer:
+class KBinsDiscretizer:
     @staticmethod
-    def _transform(X, n_bins):
-        return KBinsDiscretizer(n_bins=n_bins, encode='ordinal', strategy='uniform')\
-            .fit_transform(X)
+    def _transform(X, n_bins, method):
+        """
+        Apply discretizations from scikit-learn
+
+        Args:
+            X: 1-D array, data to be descretized
+            n_bins: int, number of bins
+            method: string, method used by scikit-learn's KBinsDiscretizer. Either 'uniform', 'quantile' or 'kmeans'
+
+        Returns:
+            1-D array, transformed data
+
+        References:
+            [1] https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.KBinsDiscretizer.html
+        """
+        return sklearn.preprocessing.KBinsDiscretizer(n_bins=n_bins, encode='ordinal', strategy=method).fit_transform(X)
 
 
-class EqualFrequencyDiscretizer:
+class CARTDiscretizer:
     @staticmethod
-    def _transform(X, n_bins):
-        return KBinsDiscretizer(n_bins=n_bins, encode='ordinal', strategy='quantile')\
-            .fit_transform(X)
+    def _transform(X, y, min_samples_leaf=None):
+        """
+        Apply CART discretization
 
+        Args:
+            X: 1-D array, data to be descretized
+            y: 1-D array, target values
+            min_samples_leaf: The minimum number of samples required to be at a leaf node. A split point at any depth will
+            only be considered if it leaves at least min_samples_leaf training samples in each of the left and right branches.
+            This may have the effect of smoothing the model, especially in regression.
+                If int, then consider min_samples_leaf as the minimum number.
+                If float, then min_samples_leaf is a fraction and ceil(min_samples_leaf * n_samples) are the minimum number of samples for each node.
+                If None, then min_samples_leaf implicitly set to 0.1
 
-class KMeansDiscretizer:
-    @staticmethod
-    def _transform(X, n_bins):
-        return KBinsDiscretizer(n_bins=n_bins, encode='ordinal', strategy='kmeans') \
-            .fit_transform(X)
+        Returns:
+            1-D array, transformed data
+
+        References:
+            [1] Liu, Huan, et al. "Discretization: An enabling technique." Data mining and knowledge discovery 6.4 (2002): 393-423.
+        """
+        min_samples_leaf = 0.1 if min_samples_leaf is None else min_samples_leaf
+
+        roc_auc_scores = []
+        for tree_depth in range(1, 6):
+            tree_model = DecisionTreeClassifier(max_depth=tree_depth, min_samples_leaf=min_samples_leaf)
+            scores = cross_val_score(tree_model, X.to_frame(), y, cv=3, scoring='roc_auc')
+            roc_auc_scores.append(np.mean(scores))
+
+        best = np.where(roc_auc_scores == np.max(roc_auc_scores))[0][0]
+
+        tree_model = DecisionTreeClassifier(max_depth=best, min_samples_leaf=min_samples_leaf)
+        tree_model.fit(X.to_frame(), y)
+
+        return tree_model.predict_proba(X.to_frame())[:, 1]
