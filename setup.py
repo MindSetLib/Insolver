fcfabdf14a33ab4ac21fd5ee7c846fe3720c8583
alexmindset <alex@mind-set.ru>
2021-11-16 12:46:03 +0300

    Remove newline from description which causes error at installation

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(name='insolver',
       version='0.4.15',
       description='Insolver is low-code machine learning library, initially created for the insurance industry, '
-                  'but can be used in any other.\n You can find a detailed overview at '
+                  'but can be used in any other. You can find a detailed overview at '
                   'https://insolver.readthedocs.io/en/latest/source/overview.html.',
       long_description=long_description,
       long_description_content_type='text/markdown',
