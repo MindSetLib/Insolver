0e5254ebd98d92b5a744688737c1b86d14a01375
alexmindset <alex@mind-set.ru>
2021-11-25 18:14:06 +0300

    Fixes in docstrings, code style and minor fixes.

--- insolver/report/Report.py
+++ insolver/report/Report.py
@@ -11,22 +11,20 @@ import jinja2
 
 
 class Report:
-    def __init__(self,
-                 model,
-                 task,
-                 X_train,
-                 y_train,
-                 predicted_train,
-                 X_test,
-                 y_test,
-                 predicted_test,
-                 directory='.',
-                 ):
-        '''
-            task        : str   "reg" or "class"
-            model       : model-file
-            directory   : str   path to templates and were to save report
-        '''
+    def __init__(self, model, task, X_train, y_train, predicted_train, X_test, y_test, predicted_test, directory='.'):
+        """Class for making html report on results.
+
+        Args:
+            model: model-file
+            task (str): "reg" or "class"
+            X_train (:obj:`pandas.DataFrame`): Dataset with X_train.
+            y_train (:obj:`pandas.Series`): Series with targets on train.
+            predicted_train (:obj:`pandas.Series`): Series with predictions on train.
+            X_test (:obj:`pandas.DataFrame`): Dataset with X_test.
+            y_test (:obj:`pandas.Series`): Series with targets on test.
+            predicted_test (:obj:`pandas.Series`): Series with predictions on test.
+            directory (str): path to templates and where to save report.
+        """
         self.model = model
         if task in ['reg', 'class']:
             self.task = task
@@ -123,69 +121,40 @@ class Report:
             if self.model.backend in ["h2o", "sklearn"]:
                 coefs = self.get_coefs_dict(self.model.coef_norm())
             elif self.model.backend in ['xgboost', 'lightgbm', 'catboost']:
-                coefs = self.get_coefs_dict(
-                                self.model.shap(
-                                    self.X_train.append(self.X_test),
-                                    show=False))
+                coefs = self.get_coefs_dict(self.model.shap(self.X_train.append(self.X_test), show=False))
             else:
-                raise Exception("Unsupperted backend type "
-                                "{}".format(self.model.backend))
+                raise Exception("Unsupperted backend type {}".format(self.model.backend))
 
-            coefs_head = ['relative_importance',
-                          'scaled_importance',
-                          'percentage']
-            model_coefs = self.create_html_table(coefs_head,
-                                                 coefs,
-                                                 two_columns_table=False,
-                                                 classes='table table-striped',
-                                                 justify='left'
-                                                 )
+            coefs_head = ['relative_importance', 'scaled_importance', 'percentage']
+            model_coefs = self.create_html_table(coefs_head, coefs, two_columns_table=False,
+                                                 classes='table table-striped', justify='left')
         else:
-            Exception("Model instance was not provided")
+            raise Exception("Model instance was not provided")
         return model_coefs
 
     def calculate_train_test_metrics(self):
-        table_train = self.calc_metrics(
-                        self.y_train,
-                        self.predicted_train,
-                        self.task)
-        table_test = self.calc_metrics(
-                        self.y_test,
-                        self.predicted_test,
-                        self.task)
+        table_train = self.calc_metrics(self.y_train, self.predicted_train, self.task)
+        table_test = self.calc_metrics(self.y_test, self.predicted_test, self.task)
 
-        table = {key: [table_train.get(key, ''), table_test.get(key, '')]
-                 for key in table_train.keys()}
-        model_metrics = self.create_html_table(["train", "test"],
-                                               table,
-                                               two_columns_table=False,
-                                               classes='table table-striped',
-                                               justify='left'
-                                               )
+        table = {key: [table_train.get(key, ''), table_test.get(key, '')] for key in table_train.keys()}
+        model_metrics = self.create_html_table(["train", "test"], table, two_columns_table=False,
+                                               classes='table table-striped', justify='left')
         return model_metrics
 
     def model_parameters_to_list(self):
         model_parameters_list = list()
         for table_name, table in self.get_objects_as_dicts(self.model):
-            model_parameters_list.append(self.create_html_table(
-                    [table_name],
-                    table,
-                    two_columns_table=True,
-                    classes='table table-striped',
-                    justify='left'
-                ))
+            model_parameters_list.append(self.create_html_table([table_name], table, two_columns_table=True,
+                                                                classes='table table-striped', justify='left'))
         return model_parameters_list
 
     def to_html(self):
-        shutil.copytree(f'{self._directory}report_template',
-                        f'{self.directory}/report')
+        shutil.copytree(f'{self._directory}report_template', f'{self.directory}/report')
         self.profile.to_file(f"{self.directory}/report/profiling_report.html")
 
         with open(f'{self.directory}/report/report.html', 'w') as f:
             html_ = self.template.render(sections=self.sections)
-            html_ = html_.replace('&#34;', '"'
-                                  ).replace('&lt;', '<'
-                                            ).replace('&gt;', '>')
+            html_ = html_.replace('&#34;', '"').replace('&lt;', '<').replace('&gt;', '>')
             f.write(html_)
 
     def get_objects_as_dicts(self, obj: any, path='') -> list:
@@ -203,90 +172,60 @@ class Report:
             return result
         elif type(obj) in [list, tuple]:
             for item in obj:
-                result.extend(self.get_objects_as_dicts(
-                    item,
-                    path=f"{path}")
-                )
+                result.extend(self.get_objects_as_dicts(item, path=f"{path}"))
         elif type(obj) in [dict]:
             for key, value in obj.items():
-                result.extend(
-                        self.get_objects_as_dicts(value, path=f'{path}/{key}'))
+                result.extend(self.get_objects_as_dicts(value, path=f'{path}/{key}'))
         elif not is_builtin(obj) and '__dict__' in dir(obj):
             if obj.__dict__:
-                result.append(
-                  (
-                    "{}/{}".format(path,
-                                   str(obj.__class__
-                                       ).replace('<', '').replace('>', '')),
-                    {key: value for key, value in obj.__dict__.items()
-                     if key[0] != '_'}
-                  )
-                )
+                result.append(("{}/{}".format(path, str(obj.__class__).replace('<', '').replace('>', '')),
+                               {key: value for key, value in obj.__dict__.items() if key[0] != '_'}))
                 result.extend(self.get_objects_as_dicts(obj.__dict__))
         return result
 
     @staticmethod
-    def create_html_table(head: list,
-                          body: dict,
-                          two_columns_table: bool = False,
-                          **kwargs
-                          ) -> str:
+    def create_html_table(head: list, body: dict, two_columns_table: bool = False, **kwargs) -> str:
         """Create html code for table based on python dict instance
 
-            arguments:
-                head:   list column's names
-                body:   dict data for table where
-                        dict keys are index names and dict values are data
-                two_columns_table:
-                        bool whether to store all data in one column
-                        or try to sparse data by columns.
-                        If True body values sholud have
-                        len(body['key']) == len(head)
-                        otherwise raised Exception
-                kwargs: dict arguments passed to
-                        DataFrame.to_html(**kwargs) method
+        Args:
+            head (list): Columns' names.
+            body (dict): Data for table, where dict keys are index names and dict values are data.
+            two_columns_table (bool): Whether to store all data in one column or try to sparse data by columns.
+             If True body values should have len(body['key']) == len(head) otherwise raised Exception.
+            **kwargs (dict): Arguments passed to pandas.DataFrame.to_html(**kwargs) method.
 
-            return:
-                str html-code for table
+        Returns:
+            (str) HTML-code for table.
+
         """
-
         def check_body(x: dict):
-            """Check if dict values are lists and all lists have same length
-            """
+            """Check if dict values are lists and all lists have same length."""
             for index, value in enumerate(body.values()):
                 if index == 0:
                     if isinstance(value, list):
                         value_len_prev = len(value)
                     else:
                         return False
-                elif not (isinstance(value, list)
-                          and len(value) == value_len_prev):
+                elif not (isinstance(value, list) and len(value) == value_len_prev):
                     return False
             return True
 
         def check_head(head, body):
-            """Checks if head list have same length with body values lists
-            """
-
+            """Checks if head list have same length with body values lists."""
             len_body_value = len(list(body.values())[0])
 
             if len_body_value != len(head):
-                raise Exception(f"column names list length {len(head)} not "
-                                "equal to columns quantity {len_body_value}")
+                raise Exception(f"column names list length {len(head)} not equal to columns quantity {len_body_value}.")
 
         if not check_body(body) or two_columns_table:
             body = {key: [value] for key, value in body.items()}
         check_head(head, body)
 
-        return pandas.DataFrame(data=body.values(),
-                                columns=head,
-                                index=body.keys()
-                                ).to_html(**kwargs)
+        return pandas.DataFrame(data=body.values(), columns=head, index=body.keys()).to_html(**kwargs)
 
     @staticmethod
     def calc_metrics(true, predicted, task):
-        """Function to calculate metrics
-        """
+        """Function to calculate metrics."""
         result = dict()
 
         metrics_regression = {
@@ -295,8 +234,7 @@ class Report:
             'mean_absolute_error': metrics.mean_absolute_error,
             'mean_squared_error': metrics.mean_squared_error,
             'median_absolute_error': metrics.median_absolute_error,
-            'mean_absolute_percentage_error':
-                metrics.mean_absolute_percentage_error,
+            'mean_absolute_percentage_error': metrics.mean_absolute_percentage_error,
             'r2_score': metrics.r2_score,
             'mean_poisson_deviance': metrics.mean_poisson_deviance,
             'mean_gamma_deviance': metrics.mean_gamma_deviance,
@@ -321,8 +259,7 @@ class Report:
             "matthews_corrcoef": metrics.matthews_corrcoef,
             "multilabel_confusion_matrix": metrics.multilabel_confusion_matrix,
             "precision_recall_curve": metrics.precision_recall_curve,
-            "precision_recall_fscore_support":
-                metrics.precision_recall_fscore_support,
+            "precision_recall_fscore_support": metrics.precision_recall_fscore_support,
             "precision_score": metrics.precision_score,
             "recall_score": metrics.recall_score,
             "roc_auc_score": metrics.roc_auc_score,
@@ -353,13 +290,9 @@ class Report:
                                     "roc_curve",
                                     ]
             else:
-                raise Exception(
-                        f"Not supported target type <{type_of_true}> "
-                        "or predicted type <{type_of_pred}>")
+                raise Exception(f"Not supported target type <{type_of_true}> or predicted type <{type_of_pred}>.")
         else:
-            raise Exception(
-                        f"Not supported task type <{task}>. "
-                        "Currently supported {['class', 'reg']}")
+            raise Exception(f"Not supported task type <{task}>. Currently supported {['class', 'reg']}.")
 
         for name in functions_names:
             try:
@@ -370,8 +303,8 @@ class Report:
 
     @staticmethod
     def get_coefs_dict(model_coefs: dict) -> dict:
-        """Extern Insolver glm wraper coef table ('relative_importance')
-           with ['scaled_importance', 'percentage'] as h2o
+        """Extract InsolverGLMWrapper coef table ('relative_importance')
+        with ['scaled_importance', 'percentage'] as h2o.
         """
         # create relative_importance value
         coefs = {key: [abs(value)] for key, value in model_coefs.items()}
@@ -385,5 +318,4 @@ class Report:
         coefs = {key: value + [value[0] / rel_imp_max, value[0] / rel_imp_sum]
                  for key, value in coefs.items()}
 
-        return {key: value for key, value
-                in sorted(coefs.items(), key=lambda x: x[1][0], reverse=True)}
+        return {key: value for key, value in sorted(coefs.items(), key=lambda x: x[1][0], reverse=True)}
