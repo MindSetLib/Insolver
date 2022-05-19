88272e31d8f82e99b72b6a98e625a176473e9099
alexmindset <alex@mind-set.ru>
2022-05-19 11:06:24 +0300

    Making InsolverDataFrame's init identical to pandas.DataFrame. Removing dummy `get_batch` method.

--- tests/test_InsolverDataFrame.py
+++ tests/test_InsolverDataFrame.py
@@ -7,7 +7,6 @@ df = pd.DataFrame({'col1': [1], 'col2': [2]})
 def test_InsolverDataFrame():
     InsDataFrame = InsolverDataFrame(df)
     assert hasattr(InsDataFrame, 'get_meta_info')
-    assert hasattr(InsDataFrame, 'get_batch')
     assert hasattr(InsDataFrame, 'split_frame')
     assert hasattr(InsDataFrame, 'sample_request')
 
