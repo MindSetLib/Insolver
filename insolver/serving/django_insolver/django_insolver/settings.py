44bfdace4697407a88572bb78b712294a34bb874
Nikolay <n.y.dodatko@gmail.com>
2022-06-17 15:34:50 +0300

    fix urls and secret

--- insolver/serving/django_insolver/django_insolver/settings.py
+++ insolver/serving/django_insolver/django_insolver/settings.py
@@ -16,7 +16,7 @@ PROJECT_ROOT = Path(__file__).resolve().parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
