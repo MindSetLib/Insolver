a766179a989b58a958f595968503a9ac5cca562a
alexmindset <alex@mind-set.ru>
2022-07-06 19:51:18 +0300

    Modify warn_insolver for static typing

--- insolver/utils.py
+++ insolver/utils.py
@@ -1,12 +1,22 @@
 import warnings
+from typing import Literal, Type, Union, Optional
 
 
-def warn_insolver(msg, category, filter_='always'):
-    def warning_format(message, category_, *args, **kwargs):
-        return f"{category_.__name__}: {message}\n"
+expected_type_warning = Literal["default", "error", "ignore", "always", "module", "once"]
+
+
+def warn_insolver(msg: Union[Warning, str],
+                  category_: Type[Warning],
+                  filter_: expected_type_warning = 'always') -> None:
+    def warning_format(message: Union[Warning, str],
+                       category: Type[Warning],
+                       filename: str,
+                       lineno: int,
+                       line: Optional[str] = None) -> str:
+        return f"{category.__name__}: {message}\n"
 
     defailt_format = warnings.formatwarning
     warnings.formatwarning = warning_format
-    warnings.simplefilter(filter_, category)
-    warnings.warn(msg, category)
+    warnings.simplefilter(filter_, category_)
+    warnings.warn(msg, category_)
     warnings.formatwarning = defailt_format
