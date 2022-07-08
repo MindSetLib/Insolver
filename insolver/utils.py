99b2b28ce832a791f8285954ffed96e1c9af2065
alexmindset <alex@mind-set.ru>
2022-07-08 18:18:15 +0300

    Adding Literal support for python 3.7

--- insolver/utils.py
+++ insolver/utils.py
@@ -1,13 +1,16 @@
 import warnings
-from typing import Literal, Type, Union, Optional
+from typing import Type, Union, Optional
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
 
 
 expected_type_warning = Literal["default", "error", "ignore", "always", "module", "once"]
 
 
 def warn_insolver(msg: Union[Warning, str],
-                  category_: Type[Warning],
-                  filter_: expected_type_warning = 'always') -> None:
+                  category_: Type[Warning]) -> None:
     def warning_format(message: Union[Warning, str],
                        category: Type[Warning],
                        filename: str,
@@ -15,7 +18,7 @@ def warn_insolver(msg: Union[Warning, str],
                        line: Optional[str] = None) -> str:
         return f"{category.__name__}: {message}\n"
 
-    defailt_format = warnings.formatwarning
+    default_format = warnings.formatwarning
     warnings.formatwarning = warning_format
     warnings.warn(msg, category_)
-    warnings.formatwarning = defailt_format
+    warnings.formatwarning = default_format
